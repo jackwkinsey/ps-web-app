d02b8a1faedbd02f1c0d992e72cf012bb3c6639a
Jack Kinsey <jack.kinsey@technivation.com>
2019-05-22 11:51:15 -0500

    Add project files.

---
+++ SampleAppTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SampleAppTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
